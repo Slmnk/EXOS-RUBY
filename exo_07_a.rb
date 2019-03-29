ac36365b415e1f01f82cbcde167f5aaae2890263
Solomonk <kicshary@gmail.com>
2019-03-29 13:53:03 +0100

    ajout commentaire

--- exo_07_a.rb
+++ exo_07_a.rb
@@ -3,4 +3,4 @@ user_name = gets.chomp
 puts user_name
 
 #gets.chomp correspond à gets, qui nous renvoie une chaîne de caractère (donnée String) et chomp est une méthode de la classe string.
-#cela permet donc d'associer une variable, par exemple, user, à gets.chomp, pour que ce dernier renvoie l'information lorsque l'on utilise gets.chomp
+#cela permet donc d'associer une variable, par exemple, ici user_name, à gets.chomp, pour que ce dernier renvoie l'information (la chaine de caractères) lorsque l'on sollicite la variable
