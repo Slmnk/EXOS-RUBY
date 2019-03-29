42ff00ffb6d720cb8022ae451486585c2b7d35e8
Solomonk <kicshary@gmail.com>
2019-03-29 23:30:10 +0100

    recréation du repo car j'ai fail

--- ruby_09.rb
+++
@@ -1,7 +0,0 @@
-puts "Bonjour, c'est quoi ton blase ?"
-print "> "
-user_name = gets.chomp
-puts "Et ton nom de famille?"
-print "> "
-user_name2 = gets.chomp
-puts "Bonjour, " + user_name +  " " +  user_name2 + " !"
