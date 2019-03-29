9b184eb8e047e88aff11cfffd032907f122db539
Solomonk <kicshary@gmail.com>
2019-03-29 13:26:36 +0100

    ex 5, ajout commentaires

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #ajoute un message
+puts "Travail : #{10 * 5 * 11}" # #{} sert à afficher le contenu d'une variable, les * à multiplier
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #ici multiplication de plusieurs valeurs différentes
+
+puts "Et en secondes ?" #ajoute un message
+
+puts 10 * 5 * 11 * 60 * 60 # affiche le résultat d'une multiplication
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #ajoute un message
+
+puts 3 + 2 < 5 - 7 #ici on voit si 3+2 est inférieur à 5-7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # addition basique
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # soustraction
+
+puts "Ok, c'est faux alors !" #ajoute un message
+
+puts "C'est drôle ça, faisons-en plus :" #ajoute un message
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # on voit si 5 est plus grand que -2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # on voit si 5 est supérieur ou égal à -2, > ou < signifiant supérieur/inférieur et le = représentant l'égalité
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # pareil cette fois ci mais on veut savoir si 5 est inférieur ou égal à -2.
