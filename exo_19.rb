bd6031a491e3341a74e3c84afc5e029669066bbe
Solomonk <kicshary@gmail.com>
2019-03-29 23:00:04 +0100

    ex 19 pas réussi

---
+++ exo_19.rb
@@ -0,0 +1,13 @@
+array_50 = []
+
+fake_email = 1.upto(50) do |iterating_email|
+    if iterating_email <10
+        array_50 << "jean.dupont.0#{iterating_email}@email.fr"
+    else
+        array_50 << "jean.dupont.#{iterating_email}@email.fr"
+    end
+end
+
+print array_50
+
+#même code que l'exo 18, je n'ai pas réussi
