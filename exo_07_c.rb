f54a982180f52b298d4e5070b3625591942b33c7
Solomonk <kicshary@gmail.com>
2019-03-29 13:53:28 +0100

    ex 7c

---
+++ exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+#ici, il n'y a plus de message d'accueil, sa seule fonction est du coup, lorsque le l'utilisateur écrit une chaine de caractères, cela va renvoyer automatiquement cette même chaine de caractères, sans rien faire d'autre.
