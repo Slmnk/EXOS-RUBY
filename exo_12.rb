d7be179cded33b4031a398a6b6046f37839784b1
Solomonk <kicshary@gmail.com>
2019-03-29 17:24:06 +0100

    ex 12

---
+++ exo_12.rb
@@ -0,0 +1,9 @@
+puts "Salut toi! Choisis un chiffre"
+print "> "
+number = gets.to_i
+
+number.times do |i|
+    puts "#{i}"
+end
+
+#je ne sais pas si l'exercice est bien fait ou non, si je tape 9 ça va compter jusqu'à 8, pas jusqu'à 9, pourtant il me semble bien que c'est le plus approprié, je comprends pas pourquoi ça va pas jusque 9.
